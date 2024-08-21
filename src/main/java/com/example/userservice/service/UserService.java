fdbf9e93a56c77592a004130b03c6c66403bf9b8
jisoo <q33asq33as@gmail.com>
2024-08-21 22:02:10 +0900

    feat: spring security

--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -2,10 +2,11 @@ package com.example.userservice.service;
 
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.jpa.UserEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     UserDto createUser(UserDto userDto);
     UserDto getUserByUserId(String userId);
     List<UserEntity> getUserByAll();
