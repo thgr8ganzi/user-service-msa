2b4cae324507ba2ea86286bee1b5fe7ea5b0a914
jisoo <q33asq33as@gmail.com>
2024-08-26 22:27:22 +0900

    feat: spring cloud config DB 적용

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -36,6 +36,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests().antMatchers("/actuator/**").permitAll();
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
         http.authorizeRequests()
                 .antMatchers("/**")
                 .hasIpAddress("192.168.0.9")
