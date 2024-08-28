8c4fc98093cbed229025273e4f23085b4a6161c7
jisoo <q33asq33as@gmail.com>
2024-08-28 22:00:51 +0900

    feat: FeignClient

---
+++ src/main/java/com/example/userservice/error/FeignErrorDecoder.java
@@ -0,0 +1,31 @@
+package com.example.userservice.error;
+
+import feign.Response;
+import feign.codec.ErrorDecoder;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+@Component
+public class FeignErrorDecoder implements ErrorDecoder {
+    private final Environment env;
+    public FeignErrorDecoder(Environment env) {
+        this.env = env;
+    }
+    @Override
+    public Exception decode(String s, Response response) {
+        switch (response.status()) {
+            case 400:
+                break;
+            case 404:
+                if (s.contains("getOrders")) {
+                    return new ResponseStatusException(HttpStatus.valueOf(response.status()), env.getProperty("order_service.exception.order_is_empty"));
+                }
+                break;
+            default:
+                return new Exception(response.reason());
+        }
+        return null;
+    }
+}
